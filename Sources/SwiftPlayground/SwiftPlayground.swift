5496420e3843f405f5020567caf160642b39a0c1
Oc-Josh-Evans-25 <josh.evans@student.onslow.school.nz>
2026-02-18 22:13:24 +0000

    Progress save on challenge

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,32 +1,27 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-func showArea() {
-    let width = 3
-    let length = 4
-    let area = width * length
-    print(area)
+func menuChoice() {
+    var userChoice = 0
+    print("==== Egg Shop ====")
+    print("   1. Add eggs")
+    print("   2. Sell eggs")
+    print("   3. Show current stock")
+    print("   4. Show total eggs sold")
+    print("   5. Exit")
+    print("   Choose an option:")
+    if let choice = readLine(), let choiceNumber = Int(choice) {
+        if choiceNumber == 1 {
+            userChoice = choiceNumber
+        } else {
+            print("Invalid choice. Please enter number from choices 1-5.")
+        } 
+    }
 }
 
 @main
 struct SwiftPlayground {
     static func main() {
-        if true {
-            let note = "Inside"
-            print(note)
-        }
-
-        // print(note) // Outside if statement it was defined in
-
-        var status = ""
-
-        if true {
-            status = "true"
-            print(status)
-        }
-
-        print(status)
-
-        print(showArea())
+        
     }
 }
