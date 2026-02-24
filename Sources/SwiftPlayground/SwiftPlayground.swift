f761fcec4d57d7ace741e33371cf738b4f4eb9fb
Oc-Josh-Evans-25 <josh.evans@student.onslow.school.nz>
2026-02-24 00:12:32 +0000

    Finished 2-d array task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,38 +1,93 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-func menuChoice() {
-    print("==== Egg Shop ====")
-    print("   1. Add eggs")
-    print("   2. Sell eggs")
-    print("   3. Show current stock")
-    print("   4. Show total eggs sold")
-    print("   5. Exit")
-    print("   Choose an option:")
-    if let choice = readLine(), let choiceNumber = Int(choice) {
-        if choiceNumber == 1 {
-            print("How many eggs do you want to add?") 
-            let amountAdding = Int(readLine()!)!
-            addEggs(currentStock: 0, amount: amountAdding)
-        } else {
-            print("Invalid choice. Please enter number from choices 1-5.")
-        } 
+func print(board: [[String]]) {
+    board.forEach{ line in 
+        print("\(line[0]) | \(line[1]) | \(line[2])")
+        print("--+---+--")
     }
+    print()
 }
 
-func addEggs(currentStock: Int, amount: Int) -> Int {
-    return currentStock + amount
+func askForPosition(board: [[String]]) -> [Int] {
+    while true {
+        print("Please enter the row number 1-3: ")
+        if let userInput = readLine(), var rowNumber = Int(userInput) {
+            print("Please enter the column number 1-3: ")
+            rowNumber = rowNumber - 1
+            if let userInput = readLine(), var columnNumber = Int(userInput) {
+                columnNumber = columnNumber - 1
+                if board[rowNumber][columnNumber] == "." {
+                    return [rowNumber, columnNumber]
+                }
+            }
+        }
+    }
 }
 
 @main
 struct SwiftPlayground {
     static func main() {
-        var eggsInStock = 0
-        var eggsSold = 0
-        
-        var menuRunning = true
-        while menuRunning == true {
-            menuChoice()
+        var player = "O"
+        var gameRunning = true
+
+        var board = [
+            [".",".","."], // row 0
+            [".",".","."], // row 1
+            [".",".","."] // row 2
+        ]
+        print(board: board)
+
+
+        while gameRunning == true {
+            // Ask for user's move.
+            let position = askForPosition(board: board)
+            board[position[0]][position[1]] = player
+            
+            if player == "O" {
+                player = "X"
+            } else if player == "X" {
+                player = "O"
+            }
+
+            print(board: board)
         }
+        // // First move: 0 in middle.
+        // board[1][1] = "O"
+        // print(board: board)
+
+        // // Second move: X in the top left.
+        // board[0][0] = "X"
+        // print(board: board)
+
+        // // Third move: 0 in the top right.
+        // board[0][2] = "O"
+        // print(board: board)
+
+        // // Fourth move: X in the bottom left.
+        // board[2][0] = "X"
+        // print(board: board)
+
+        // // Fifth move: 0 in middle left.
+        // board[1][0] = "O"
+        // print(board: board)
+
+        // // Sixth move: X in middle right.
+        // board[1][2] = "X"
+        // print(board: board)
+
+        // // Seventh move: 0 in bottom middle.
+        // board[2][1] = "O"
+        // print(board: board)
+
+        // // Eighth move: X in bottom right.
+        // board[2][2] = "X"
+        // print(board: board)
+
+        // // Final move: O in top middle.
+        // board[2][0] = "O"
+        // print(board: board)
+
+
     }
 }
