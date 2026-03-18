2a00998b31baa93c73c8bbad07f2ec1f3eab7162
Oc-Josh-Evans-25 <josh.evans@student.onslow.school.nz>
2026-03-18 21:21:46 +0000

    Mar 18, 2026, 9:21 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -99,8 +99,8 @@ struct SwiftPlayground {
         for i in 1...maximumGuesses {
             print("Please enter a row number: ")
             if let userInput = readLine(), let row = Int(userInput), row >= 1, row <= size {
-               print("Please enter a col number: ")
-               if let userInput2 = readLine(), let col = Int(userInput2), col >= 1, col <= size {
+                print("Please enter a col number: ")
+                if let userInput2 = readLine(), let col = Int(userInput2), col >= 1, col <= size {
                     let newGuesses = processGuess(row: row, col: col, ocean: ocean, guesses: guesses)
                     printBoard(guesses)
                 } 
