c1eea6162a3284bf253576e3e0ae98d87d9cbfca
Oc-Josh-Evans-25 <josh.evans@student.onslow.school.nz>
2026-03-24 00:14:40 +0000

    Saving changes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,96 +1,54 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-func print(board: [[String]]) {
-    board.forEach{ line in 
-        print("\(line[0]) | \(line[1]) | \(line[2])")
-        print("--+---+--")
-    }
-}
-
-func askForPosition(board: [[String]]) -> [Int] {
-    while true {
-        print("Please enter the row number 1-3: ")
-        if let userInput = readLine(), var rowNumber = Int(userInput) {
-            print("Please enter the column number 1-3: ")
-            rowNumber = rowNumber - 1
-            if let userInput = readLine(), var columnNumber = Int(userInput) {
-                columnNumber = columnNumber - 1
-                if board[rowNumber][columnNumber] == "." {
-                    return [rowNumber, columnNumber]
-                } else {
-                    print("A naught or a cross has already been placed in that position. \n")
-                }
-            }
-        }
-    }
-}
+import Foundation
 
 @main
 struct SwiftPlayground {
     static func main() {
-        var player = "O"
-        var gameRunning = true
-
-        var board = [
-            [".",".","."], // row 0
-            [".",".","."], // row 1
-            [".",".","."] // row 2
+        // Constants and variables.
+        
+        /// The vocalubary to test the user on.
+        /// Each inner array contains the English, word, the correct answer in the
+        /// target language, and then three incorect answers.
+        let vocabulary = [
+            ["Hello", "Kia ora", "Ka pai", "Ata marie", "Kao"],
+            ["Goodbye", "Ata marie", "Kia ora", "Ae", "Haere mai"],
+            ["Thank you", "Kia ora", "Kao", "Ata marie", "Ae"],
+            ["Yes", "Ae", "Haere ra", "Ata marie", "Kao"],
+            ["No", "Kao", "Ka pai", "Haere ra", "Ae"]
         ]
-        print(board: board)
 
+        /// The indices of the questions that user got wrong.
+        var incorrectIndices: [Int] = []
 
-        while gameRunning == true {
-            // Ask for user's move.
-            let position = askForPosition(board: board)
-            board[position[0]][position[1]] = player
+        /// The number of questions that user got wrong first time around.
+        var incorrectCount = 0
+
+        /// The number of questions the user has been asked.
+        var count = 0
+
+        // Loop until all of the vocabulary questions have been asked.
+        vocabulary.enumerated().forEach { index, word in
+            // Show the question.
+            print("Translate this word: \(vocabulary[index][0])")            
             
-            if player == "O" {
-                player = "X"
-            } else if player == "X" {
-                player = "O"
+            // Present the possible answers.
+            print("1. \(vocabulary[index][1])")
+            print("2. \(vocabulary[index][2])")
+            print("3. \(vocabulary[index][3])")
+            print("4. \(vocabulary[index][4])")
+
+            print("\n Enter your guess for \(vocabulary[index][0]): ")
+
+            if let response = readLine(), let userResponse = Int(response) {
+                
             }
 
-            print(board: board)
+            // Check if the user guessed the correct answer.
+            // If not, make a note of the question to ask again later.
         }
+
         
-
-        // // First move: 0 in middle.
-        // board[1][1] = "O"
-        // print(board: board)
-
-        // // Second move: X in the top left.
-        // board[0][0] = "X"
-        // print(board: board)
-
-        // // Third move: 0 in the top right.
-        // board[0][2] = "O"
-        // print(board: board)
-
-        // // Fourth move: X in the bottom left.
-        // board[2][0] = "X"
-        // print(board: board)
-
-        // // Fifth move: 0 in middle left.
-        // board[1][0] = "O"
-        // print(board: board)
-
-        // // Sixth move: X in middle right.
-        // board[1][2] = "X"
-        // print(board: board)
-
-        // // Seventh move: 0 in bottom middle.
-        // board[2][1] = "O"
-        // print(board: board)
-
-        // // Eighth move: X in bottom right.
-        // board[2][2] = "X"
-        // print(board: board)
-
-        // // Final move: O in top middle.
-        // board[2][0] = "O"
-        // print(board: board)
-
-
     }
 }
