2bc2625efbc813947f193717e1bc218e389d7c30
Oc-Josh-Evans-25 <josh.evans@student.onslow.school.nz>
2026-04-22 22:50:18 +0000

    Saving Changes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -3,14 +3,37 @@
 
 import Foundation
 
+/// Gets user response and checks whether number is valid.
+/// - Parameters:
+///   - error: Error message if value is invalid.
+///   - minNumber: smallest valid number option from the menu.
+///   - maxNumber: largest valid number option from the menu.
+/// - Returns: 
+func userResponse(error: String, minNumber: Int, maxNumber: Int) -> Int {
+    var gettingResponse = true
+    while gettingResponse {
+        if let response = readLine(), let optionResponse = Int(response) {
+            if optionResponse >= minNumber && optionResponse <= maxNumber {
+                gettingResponse = false
+                return optionResponse
+            } else {
+                print(error)
+            }
+        } else {
+            print(error)
+        }
+    }
+}
+
 func menuChoice() -> Int {
-    print("==== Egg Shop ====")
-    print("   1. Add eggs")
-    print("   2. Sell eggs")
+    print("==== Kumara Stall ====")
+    print("   1. Buy Kumara")
+    print("   2. Sales Records")
     print("   3. Show current stock")
-    print("   4. Show total eggs sold")
-    print("   5. Exit")
-    print("   Choose an option:")
+    print("   4. Summary information")
+    print("   5. Add Kumara to stock")
+    print("   6. Exit")
+    print("   Choose an option 1-6:")
     let menu = userResponse(error: "Invalid number. Please enter a number from 1 to 5.", minNumber: 1, maxNumber: 5)
     return menu
 }
@@ -18,6 +41,6 @@ func menuChoice() -> Int {
 @main
 struct SwiftPlayground {
     static func main() {
-       
+        
     }
 }
