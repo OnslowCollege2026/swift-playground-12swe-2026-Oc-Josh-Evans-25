f666f7e350866bc26fd225021366e11eea015a3d
Oc-Josh-Evans-25 <josh.evans@student.onslow.school.nz>
2026-05-13 22:37:49 +0000

    Temporary Save

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -83,32 +83,33 @@ struct SwiftPlayground {
                 if kumaraStock > 0 {
                     print("Kumara are $3 per kilogram.")
                     print("How many Kilograms of Kumara do you want to buy?")
-                    if let input = readLine(), let kumaraWeight = Double(input), kumaraWeight <= kumaraStock {
-                        sales[0].append(kumaraWeight)
-                        print("How many bags would you like? Each bag can hold 5kg of Kumara.")
-                        if let input2 = readLine(), let bagAmount = Double(input2), bagAmount <= bagStock {
-                            if bagAmount < kumaraWeight / 5 {
-                                print("Invalid amount of bags.") 
-                                print("Please check you have at least 1 bag per 5kg of Kumara and try again.")
-                            } else {
-                                sales[1].append(bagAmount)
-                                let saleResult = kumaraSale(kumaraCost: kumaraCost, bagCost: bagCost, kumaraWeight: kumaraWeight, bagAmount: bagAmount)
-                                sales[2].append(saleResult) 
-                                print(sales)
-                            }
-                        } 
+                    guard let input = readLine(), let kumaraWeight = Double(input), kumaraWeight <= kumaraStock else {
+                        return
                     }
-                }
+                    sales[0].append(kumaraWeight)
+                    print("How many bags would you like? Each bag can hold 5kg of Kumara.")
+                    guard let input2 = readLine(), let bagAmount = Double(input2), bagAmount <= bagStock else {
+                        return
+                    }
+                    if bagAmount < kumaraWeight / 5 {
+                        print("Invalid amount of bags.") 
+                        print("Please check you have at least 1 bag per 5kg of Kumara and try again.")
+                    } else {
+                        sales[1].append(bagAmount)
+                        let saleResult = kumaraSale(kumaraCost: kumaraCost, bagCost: bagCost, kumaraWeight: kumaraWeight, bagAmount: bagAmount)
+                        sales[2].append(saleResult)
+                    }
+                } 
             } else if menuOption == 2 {
                 let numberInColumn = sales[0].count
                 print("These are sales that have been made so far: \n")
 
-                for i in 0..<numberInColumn {
-                    print("Sale \(i + 1): ")
+                for col in 0..<numberInColumn {
+                    print("Sale \(col + 1): ")
                     
-                    let kumaraWeightRow = sales[0]
-                    let bagAmountRow = sales[1]
-                    let saleCostRow = sales[2]
+                    let kumaraWeightRow = sales[0][col]
+                    let bagAmountRow = sales[1][col]
+                    let saleCostRow = sales[2][col]
 
                     print("Purchase amount: $\(saleCostRow)")
                     print("Kumara Weight Bought: \(kumaraWeightRow)kg ")
