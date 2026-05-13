9c013d8613d4d9dea675f784de7ebe74efa158f2
Oc-Josh-Evans-25 <josh.evans@student.onslow.school.nz>
2026-05-13 23:30:50 +0000

    Still finishing comments.

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -8,7 +8,7 @@ import Foundation
 ///   - error: Error message if value is invalid.
 ///   - minNumber: smallest valid number option from the menu.
 ///   - maxNumber: largest valid number option from the menu.
-/// - Returns: 
+/// - Returns: The user input as an int.
 func userResponse(error: String, minNumber: Int, maxNumber: Int) -> Int {
     var gettingResponse = true
     while gettingResponse {
@@ -25,6 +25,8 @@ func userResponse(error: String, minNumber: Int, maxNumber: Int) -> Int {
     }
 }
 
+/// Uses userResponse function to let them choose which option by entering a number from 1 to 6.
+/// Returns: 
 func menuChoice() -> Int {
     print("==== Kumara Stall ====")
     print("   1. Make a sale")
@@ -38,6 +40,13 @@ func menuChoice() -> Int {
     return menu
 }
 
+/// Calculates how much the total cost of Kumaras and bags.
+/// -  Parameters:
+///   - kumaraCost: Default kumaraCost
+///   - bagCost: Default bagCost
+///   - kumaraWeight: How much weight the user is buying inputted into the function.
+///   - bagAmount: Amount of bags the user is buying.
+/// Returns: The total cost based on how Kumara they're buying and how many bags they're buying.
 func kumaraSale(kumaraCost: Double, bagCost: Double, kumaraWeight: Double, bagAmount : Double) -> Double {
     let totalBagCost = bagAmount * bagCost
     let totalCost = (kumaraWeight * kumaraCost) + totalBagCost
@@ -45,6 +54,11 @@ func kumaraSale(kumaraCost: Double, bagCost: Double, kumaraWeight: Double, bagAm
     return totalCost
 } 
 
+/// Gets the total of a row in a multidimensional array.
+/// -  Parameters:
+///    - matrix: the 2d array being used
+///    - row: which row to get the total from
+/// Returns: The total value of the whole row added together.
 func rowTotal(in matrix: [[Double]], row: Int) -> Double {
     guard row >= 0 && row < matrix.count else { return 0 }
     return matrix[row].reduce(0, +)
@@ -79,21 +93,34 @@ struct SwiftPlayground {
         // Loop that runs until the user chooses to exit.
         var menuRunning = true
         while menuRunning == true {
+            // Calls the menu choice function to let the user choose which option to use.
             let menuOption = menuChoice()
+            
+            // Accords to the make a sale option in the menu.
             if menuOption == 1 {
+                // Ensures that there is still stock left to buy, gives user an error if it isn't.
                 if kumaraStock > 0 {
+                    // Gets user input for how much Kumara they want to buy and gives them an error if they enter a number lower than 0.
                     print("Kumara are $3 per kilogram.")
                     print("How many Kilograms of Kumara do you want to buy?")
                     guard let input = readLine(), let kumaraWeight = Double(input), kumaraWeight > 0, kumaraWeight <= kumaraStock else {
                         print("Please enter a valid number of Kumara that you want to buy.")
                         return
                     }
+                    // If there is no error, the program proceeds and the kumaraWeight is added into the first row of the sales 2d Array.
                     sales[0].append(kumaraWeight)
-                    print("How many bags would you like? Each bag can hold 5kg of Kumara.")
-                    guard let input2 = readLine(), let bagAmount = Double(input2), bagAmount <= bagStock else {
+
+                    // Gets user input for amount of bags the user is buying 
+                    print("How many bags would you like? Each bag can hold 5kg of Kumara.") 
+                    guard let input2 = readLine(), let bagAmount = Double(input2), bagAmount > 0, bagAmount <= bagStock else {
+                        print("Invalid amount of bags.") 
+                        print("Please check you have at least 1 bag per 5kg of Kumara and try again.")
                         return
                     }
 
+                    // Checks if user has too much Kumara in one bag, the limit is 5kg per bag.
+                    // If there is no problem, the program adds the amount of bags they bought 
+                    // and how much the total sale costed into seperate rows of the 2d sales array.
                     if bagAmount < kumaraWeight / 5 {
                         print("Invalid amount of bags.") 
                         print("Please check you have at least 1 bag per 5kg of Kumara and try again.")
@@ -105,7 +132,10 @@ struct SwiftPlayground {
                 } else {
                     print("Stock is empty.")
                 }
+            
+            // Accords to the sale records option in the menu.
             } else if menuOption == 2 {
+                // Finds out how many columns there are.
                 let numberInColumn = sales[0].count
                 print("These are sales that have been made so far: \n")
 
