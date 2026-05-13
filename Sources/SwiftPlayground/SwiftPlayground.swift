8d839d99769ff17ab5ad2a488def69d259a8eee9
Oc-Josh-Evans-25 <josh.evans@student.onslow.school.nz>
2026-05-13 10:13:12 +0000

    Save

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,12 +1,22 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+func rowTotal(in matrix: [[Int]], row: Int) -> Int {
+    guard row >= 0 && row < matrix.count else { return 0 }
+    return matrix[row].reduce(0, +)
+}
+
 @main
 struct SwiftPlayground {
     static func main() {
-        // var sales: [[Double]] = [[], [], []]
+        var sales: [[Double]] = [[], [], []]
         
-        // let bagStock = 5000.0
+        let bagStock = 5000.0
+
+        var averagePrice: Int
+
+        averagePrice = rowTotal(in: sales, row: averagePrice)
+        print(averagePrice)
 
         // print("How many kilograms of Kumara do you want to buy?")
         // guard let input = readLine(),
@@ -29,31 +39,31 @@ struct SwiftPlayground {
         //     print(sales)
         // }
 
-        let salesData: [[Int]] = [
-        [10, 20, 30], // Day 1
-        [15, 25, 35], // Day 2
-        [20, 30, 40]  // Day 3
-        ]
+//         let salesData: [[Int]] = [
+//         [10, 20, 30], // Day 1
+//         [15, 25, 35], // Day 2
+//         [20, 30, 40]  // Day 3
+//         ]
 
-        let numRows = salesData.count
-        let numCols = salesData[0].count
+//         let numRows = salesData.count
+//         let numCols = salesData[0].count
 
-        print("--- Printing Column by Column ---")
+//         print("--- Printing Column by Column ---")
         
-        // Outer loop: Columns
-        for col in 0..<numCols {
-            print("Sale \(col + 1):")
+//         // Outer loop: Columns
+//         for col in 0..<numCols {
+//             print("Sale \(col + 1):")
             
-            let price = salesData[0][col]
-            let weight = salesData[1][col]
-            let bags = salesData[2][col]
+//             let price = salesData[0][col]
+//             let weight = salesData[1][col]
+//             let bags = salesData[2][col]
 
-            print("Price: \(price)")
-            print("WeightBought: \(weight)")
-            print("BagsBought: \(bags) \n")
-        }
-    }
-}
+//             print("Price: \(price)")
+//             print("WeightBought: \(weight)")
+//             print("BagsBought: \(bags) \n")
+//         }
+//     }
+// }
 
 // if kumaraStock > 0 {
 //                     print("Kumara are $3 per kilogram.")
