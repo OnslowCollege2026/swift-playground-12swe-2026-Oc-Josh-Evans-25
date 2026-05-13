1c7d5090968a197314d10d20ae0bfa63831ab625
Oc-Josh-Evans-25 <josh.evans@student.onslow.school.nz>
2026-05-13 10:50:27 +0000

    Finished

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -69,22 +69,3 @@ struct SwiftPlayground {
         //     print("BagsBought: \(bags) \n")
         }
     }
-
-// if kumaraStock > 0 {
-//                     print("Kumara are $3 per kilogram.")
-//                     print("How many Kilograms of Kumara do you want to buy?")
-//                     if let input = readLine(), let kumaraWeight = Double(input), kumaraWeight <= kumaraStock {
-//                         sales[0].append(kumaraWeight)
-//                         print("How many bags would you like? Each bag can hold 5kg of Kumara.")
-//                         if let input2 = readLine(), let bagAmount = Double(input2), bagAmount <= bagStock {
-//                             if bagAmount < kumaraWeight / 5 {
-//                                 print("Invalid amount of bags.") 
-//                                 print("Please check you have at least 1 bag per 5kg of Kumara and try again.")
-//                             } else {
-//                                 sales[1].append(bagAmount)
-//                                 let saleResult = kumaraSale(kumaraCost: kumaraCost, bagCost: bagCost, kumaraWeight: kumaraWeight, bagAmount: bagAmount)
-//                                 sales[2].append(saleResult) 
-//                                 print(sales)
-//                             }
-//                         } 
-//                     }
